64b532d53d4799958da5d3016193aac6696456de
kokipett <152555780+kokipett@users.noreply.github.com>
2024-05-02 14:12:17 -0700

    Create FreeNode.s

---
+++ FreeNode.s
@@ -0,0 +1,55 @@
+.global FreeNodes
+
+   .data
+
+//x0: =headptr
+
+   .text
+FreeNodes:
+   stp   x19, x20, [sp, #-16]!   // Push x19 and x20, then move SP down 16 bytes
+   stp   x21, x22, [sp, #-16]!   // Push x21 and x22, then move SP down 16 bytes
+   stp   x23, x24, [sp, #-16]!   // Push x23 and x24, then move SP down 16 bytes
+   stp   x25, x26, [sp, #-16]!   // Push x25 and x26, then move SP down 16 bytes
+   stp   x27, x28, [sp, #-16]!   // Push x27 and x28, then move SP down 16 bytes
+   stp   x29, x30, [sp, #-16]!   // Push x29 and x30, then move SP down 16 bytes
+
+
+    mov X19,#0
+    mov X20,#0
+
+//	ldr	x0,[x0]
+    mov X19,x0
+    LDR x19,[x19]
+
+
+    Loop:
+    CMP x19,#0
+    BEQ end
+
+    MOV x20,X19      //safely store node
+    add x19,x19,#8
+
+    ldr x19, [x19]
+    ldr x0,[x20]
+    BL  free
+
+    mov x0,x20
+    BL free
+
+    b   Loop    
+
+
+
+end:
+
+   ldp   x29, x30, [sp], #16     // Pop x29 and x30, then move SP up 16 bytes
+   ldp   x27, x28, [sp], #16     // Pop x27 and x28, then move SP up 16 bytes  
+   ldp   x25, x26, [sp], #16     // Pop x25 and x26, then move SP up 16 bytes
+   ldp   x23, x24, [sp], #16     // Pop x23 and x24, then move SP up 16 bytes
+   ldp   x21, x22, [sp], #16     // Pop x21 and x22, then move SP up 16 bytes
+   ldp   x19, x20, [sp], #16     // Pop x19 and x20, then move SP up 16 bytes
+
+
+   RET                           // Return to caller
+
+   .end
